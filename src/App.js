b287dbac6ed960cbdad5070c1a0bcc831e7282b2
Sanskar <sanskarsontakke@gmail.com>
2025-05-01 21:28:02 +0530

    modified:   src/App.js 	modified:   src/components/navbar.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import './App.css'; // Or your preferred styling
 import Navbar from './components/navbar';
 import Card from './components/cards/cards';
 import CarouselMain from './components/carousel/carousel';
-import { useState } from 'react';
+import { useRef, useState, useEffect } from 'react';
 
 function App() {
   const [currentPage, setCurrentPage] = useState(0);
 
   const handlePageChange = (newPage) => {
     setCurrentPage(newPage);
-    console.log("Page changed to:", newPage);
+    console.log("Current page changed to:", newPage);
   };
+
+  useEffect(() => {
+    console.log("Current page:", currentPage);
+  }, [currentPage]);
 
   return (
     <div>
